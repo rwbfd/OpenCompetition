759a06bea3f8c9e195a1ba34fc6ec945f5149e43
rwbfd <54792996+rwbfd@users.noreply.github.com>
2020-01-08 14:57:42 +0800

    Add files via upload

--- src/tabular/feature_engineering/new_featureEngeneer/mean_encoder.py
+++ src/tabular/feature_engineering/new_featureEngeneer/mean_encoder.py
@@ -1,12 +1,21 @@
+import numpy as np
 
-def mean_encoder(dfs, target, encoded_vars, encoded_methods):
-    '''
-    
-    :param dfs: Contains a list of cross validated set.
-    :param target: the list of target variables to encode from
-    :param encoded_vars: the list of encoded method
-    :param encoded_methods: mean or something
-    :return: A list of dfs
-    '''
 
+def mean_encoder(df_list, encoded_vars, target, method):
+    '''
+    :param df_list: 
+    :param encoded_vars: 需要编码的变量
+    :param target: 预测变量
+    :param method: mean or var
+    :return: 
+    '''
+    for A in encoded_vars:
+        if A not in df_list.cloumns or target not in df_list.cloumns:
+             raise TypeError('input valid field')
+
+        if method == 'mean':
+            return df_list[A].groupby(target).aggregate(np.mean)
+
+        elif method == 'var':
+            return df_list[A].groupby(target).aggregate(np.var)
 
