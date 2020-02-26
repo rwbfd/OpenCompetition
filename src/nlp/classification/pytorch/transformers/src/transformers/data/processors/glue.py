7176a5bdd529498861d6d12cc78d5b4f5a90c107
rwbfd <duli@oregonstate.edu>
2020-02-26 18:04:20 +0800

    Added overall process in glue.py

--- src/nlp/classification/pytorch/transformers/src/transformers/data/processors/glue.py
+++ src/nlp/classification/pytorch/transformers/src/transformers/data/processors/glue.py
@@ -165,6 +165,85 @@ def glue_convert_examples_to_features(
 
     return features
 
+class OverallProcessor(DataProcessor):
+    """Processor for the Overall data set (GLUE version)."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        if set_type is "MRPC":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, i)
+                text_a = line[3]
+                label = line[0]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        elif set_type is "MultiNLI":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, line[0])
+                text_a = line[8]
+                label = line[-1]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        elif set_type is "Cola":
+            for (i, line) in enumerate(lines):
+                guid = "%s-%s" % (set_type, i)
+                text_a = line[3]
+                label = line[1]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        elif set_type is "SST-2":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, i)
+                text_a = line[0]
+                label = line[1]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        elif set_type is "STS-B":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, line[0])
+                text_a = line[7]
+                label = line[-1]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        elif set_type is "QQP":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, line[0])
+                try:
+                    text_a = line[3]
+                    text_b = line[4]
+                    label = line[5]
+                except IndexError:
+                    continue
+                examples.append(InputExample(guid=guid, label=label,text_a=text_a))
+        elif set_type is "QNLI" or "RTE" or "WNLI":
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, line[0])
+                text_a = line[1]
+                label = line[-1]
+                examples.append(InputExample(guid=guid, label=label, text_a=text_a))
+        return examples
+
 
 class MrpcProcessor(DataProcessor):
     """Processor for the MRPC data set (GLUE version)."""
