152fada9391d062f1dd7e16efbb34f6942c939bc
rwbfd <duli@oregonstate.edu>
2020-03-03 17:55:40 +0800

    Finished first 3 function

---
+++ src/common/meta-feature/meta-feature-extraction.py
@@ -0,0 +1,95 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+class feature_extraction:
+    def __init__(self):
+        pass
+
+    def fHard(self, data_set,label_Wk, n):
+        #data_set shape will be [n_samples,shape] (k, m)
+        #label_Wk shape will be [n_smaples, 1]
+        m = data_set.shape[0]
+        K = np.zeros((1,m)) #meta feature
+        knn = KNeighborsClassifier(n_neighbors=n) #set neighbors to 3
+        knn.fit(data_set,label_Wk)
+        pred = knn.predict(data_set)
+        for i in range(data_set.shape[0]):
+            if pred[i] == label_Wk[i]:
+                K[0][i] = 1
+        return K
+
+    def fProb(self, data_set,label_Wk,n):
+        #Posterior probability： P（A|B) = P(B|A) * P(A) / P(B) [p(wl | xk)] A = wl and B = xk (p(B|A) * p(A) = p(A and B))
+        #data_set shape will be [n_samples,shape] (k, m)
+        #label_Wk shape will be [n_smaples, 1]
+        m = data_set.shape[0]
+        K = np.zeros((1,m))
+        knn = KNeighborsClassifier(n_neighbors=n) #set neighbors to 3
+        knn.fit(data_set,label_Wk)
+        pred = knn.predict(data_set) #predict list
+        #doing prob algorithm here
+        #creating list to store pwl
+        pwl = []
+        pa = []
+        tmp = 0
+        for i in range(data_set.shape[0]):
+            #counting for a classifer
+            if label_Wk[i] not in pwl:
+                pwl.append(label_Wk[i])
+        for i in range(len(pwl)):
+            for j in range(data_set.shape[0]):
+                if label_Wk[j] == pwl[i]:
+                    tmp += 1
+            pa.append(tmp/data_set.shape[0])
+            tmp = 0
+        # calculate pb
+        pxk = []
+        pb = []
+        tmp = 0
+        for i in range(data_set.shape[0]):
+            #counting for a classifer
+            if pred[i] not in pxk:
+                pxk.append(pred[i])
+        for i in range(len(pxk)):
+            for j in range(data_set.shape[0]):
+                if pred[j] == pxk[i]:
+                    tmp += 1
+            pb.append(tmp/data_set.shape[0])
+            tmp = 0
+        #shape of pa and pb are same which is equal to classes number
+        pt = (pa + pb) / 2 #pa and pb happen in the same time
+        for j in range(len(pwl))
+            for i in range(m):
+                if pwl[j] == label_Wk[i]
+                    K[0][i] = pt[j] / pb[j]
+        return K
+
+    def fOverall(self, data_set,label_Wk,n):
+        #data_set shape will be [n_samples,shape] (k, m)
+        #label_Wk shape will be [n_smaples, 1]
+        m = data_set.shape[0]
+        K = 0 #meta feature
+        knn = KNeighborsClassifier(n_neighbors=n) #set neighbors to 3
+        knn.fit(data_set,label_Wk)
+        pred = knn.predict(data_set)
+        tmp = 0
+        Wl = 0
+        for i in range(m):
+            if pred[i] == label_Wk[i]:
+                tmp += 1
+        K = tmp / m
+        return K
+
+        
+
+
+if __name__ == '__main__':
+    from sklearn import datasets
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+    # x = np.array([[0.5,0.4],[0.1,0.2],[0.7,0.8],[0.2,0.1],[0.4,0.6],[0.9,0.9],[1,1]]).reshape(-1,2)
+    # y = np.array([0,1,0,1,0,1,1]).reshape(-1,1)
+    meta = feature_extraction()
+    k = meta.fHard(x,y)
+    print('k:',k.shape, "x: ", x.shape)
